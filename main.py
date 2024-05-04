dacc110a9a1496e134a4ab10c79a95f3992542af
AlmostEvil665 <u1196904@utah.edu>
2024-05-04 01:44:15 -0600

    Added night mode theme because nice. Created navbar and added flash message support on errors and success.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.drop_submit_route import drop_submission_route
 import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'secret_hehe_uwu')
 
 @app.route('/')
 def home():
@@ -20,5 +21,6 @@ if __name__ == "__main__":
     from waitress import serve
 
     print("Starting server...")
+
     port = os.environ.get('PORT', 8080)
     serve(app, host="0.0.0.0", port=port)
--- routes/database_routes.py
+++ routes/database_routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Blueprint
+from flask import Flask, request, render_template, Blueprint, flash
 from werkzeug.utils import secure_filename
 import sqlite3
 import os
@@ -13,9 +13,9 @@ def reset_database():
         correct_password = os.environ.get('ADMIN_PASSWORD', 'password')
         if password == correct_password:
             database.reset_tables()
-            return "Database reset successfully."
+            flash("Database reset successfully.")
         else:
-            return "Incorrect password."
+            flash("Incorrect password.")
     return render_template('reset_database.html')
 
 @database_routes.route('/upload_teams', methods=['GET', 'POST'])
@@ -31,11 +31,11 @@ def upload_teams():
                 # Process the file and add the teams to the database
                 # You'll need to implement this function
                 database.read_teams(os.path.join('uploads', 'teams.csv'))
-                return "Teams uploaded successfully."
+                flash("Teams uploaded successfully.")
             else:
-                return "No file selected."
+                flash("No file selected.")
         else:
-            return "Incorrect password."
+            flash("Incorrect password.")
     return render_template('upload_teams.html')
 
 @database_routes.route('/upload_tiles', methods=['GET', 'POST'])
@@ -51,9 +51,9 @@ def upload_tiles():
                 # Process the file and add the tiles to the database
                 # You'll need to implement this function
                 database.read_tiles(os.path.join('uploads', 'tiles.csv'))
-                return "Tiles uploaded successfully."
+                flash("Tiles uploaded successfully.")
             else:
-                return "No file selected."
+                flash("No file selected.")
         else:
-            return "Incorrect password."
+            flash("Incorrect password.")
     return render_template('upload_tiles.html')
