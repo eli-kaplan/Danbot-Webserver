ecaa6a80e3a7b85fdd24f0a3b6ab91a4f994c066
AlmostEvil665 <u1196904@utah.edu>
2024-05-05 09:34:41 -0600

    Fixed unused imports

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 import os
 
 import discord
-from discord import default_permissions, guild_only
 
-import database
-import db_entities
-import utils.scapify
+
 from cogs.AdminCog import AdminCog
 from cogs.UserCog import UserCog
-from utils.autocomplete import player_names
 
 bot = discord.Bot()
 token = os.getenv('DISCORD_BOT_TOKEN')
