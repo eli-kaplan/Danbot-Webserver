9226a7546ecf887f36f9ca319d7733efd7c1514a
Danny Decker <u1196904@utah.edu>
2024-05-09 02:17:02 -0700

    Converted bot commands to link to website instead of yucky discord embeds

--- cogs/UserCog.py
+++ cogs/UserCog.py
@@ -35,127 +35,17 @@ class UserCog(commands.Cog):
     async def player(self, ctx: discord.ApplicationContext,
                      player_name: discord.Option(str, "What is the username?", autocomplete=lambda ctx: fuzzy_autocomplete(ctx, player_names()))):
         await ctx.defer()
-        player = database.get_player_by_name(player_name)
-        player = db_entities.Player(player)
-
-        embed = discord.Embed(
-            title = player.player_name,
-            description="Here's some information about your performance",
-            color=discord.Colour.yellow()
-        )
-
-        embed.add_field(name="Tile contributions", value=f"{round(player.tiles_completed, 2)}", inline=True)
-        embed.add_field(name="Gold gained", value=f"{scapify.int_to_gp(player.gp_gained)}", inline=True)
-        embed.add_field(name="Total deaths", value=f"{player.deaths}", inline=True)
-
-        drops = database.get_drops_by_player_id(player.player_id)
-
-        drop_dict = {}
-        for drop in drops:
-            drop = db_entities.Drop(drop)
-            drop_dict[drop.drop_name] = (drop.drop_quantity, drop.drop_value * drop.drop_quantity)
-
-        drop_block = "```ansi\n"
-        drop_dict = sorted(drop_dict.items(),key=lambda item: item[1][1], reverse=True)
-
-        for key, value in drop_dict:
-            spaces_needed = 56 - len(f"{value[0]} x {key}({scapify.int_to_gp(value[1])})")
-            result = f"{ftext +fred}{value[0]} x{fend} {key}{' ' * spaces_needed}{ftext + fgreen}({scapify.int_to_gp(value[1])}){fend}\n"
-            if len(drop_block) + len(result) > 1021:
-                break
-            drop_block += result
-        drop_block += "```"
-
-        kills = database.get_killcount_by_player_id(player.player_id)
-        kill_dict = {}
-        for kill in kills:
-            kill = db_entities.Killcount(kill)
-            kill_dict[kill.boss_name] = kill.kills
-
-        kill_block = "```ansi\n"
-        kill_dict = sorted(kill_dict.items(), key=lambda item: item[1], reverse=True)
-        for key, value in kill_dict:
-            spaces_needed = 56 - len(f"{key}{value}")
-            result = f"{ftext + fred}{key}{fend}{' ' * spaces_needed}{ftext + fblue}{value}\n{fend}"
-            if len(kill_block) + len(result) > 1021:
-                break
-            kill_block += result
-        kill_block += "```"
-
-        embed.add_field(name="Drops", value=drop_block, inline=False)
-        embed.add_field(name="Kills", value=kill_block, inline=False)
-        await ctx.respond(embed=embed)
+        server_ip = os.getenv('SERVER_IP')
+        player_url = f"http://{server_ip}/user/player/{player_name}"
+        await ctx.respond(player_url)
 
     @discord.slash_command(name="team", description="Get a bunch of data about a team in the bingo")
     async def team(self, ctx: discord.ApplicationContext,
                    team_name: discord.Option(str, "What is the team name?", autocomplete=lambda ctx: fuzzy_autocomplete(ctx, team_names()))):
         await ctx.defer()
-        team = database.get_team_by_name(team_name)
-        team = db_entities.Team(team)
-
-        embed = discord.Embed(
-            title = team.team_name,
-            description="Here's some information about your team performance",
-            color=discord.Colour.yellow()
-        )
-
-        total_gold = 0
-        drop_dict = {}
-        drops = database.get_drops_by_team_id(team.team_id)
-        for drop in drops:
-            drop = db_entities.Drop(drop)
-            total_gold = drop.drop_quantity * drop.drop_value + total_gold
-            drop_dict[drop.drop_name] = (drop.drop_quantity, drop.drop_value * drop.drop_quantity)
-
-
-        embed.add_field(name="Points", value=team.team_points, inline=True)
-        embed.add_field(name="Gold Gained", value=total_gold, inline=True) # Todo get gold
-        embed.add_field(name="Total deaths", value=0, inline=True) # Todo get deaths
-
-        drop_block = "```ansi\n"
-        drop_dict = sorted(drop_dict.items(),key=lambda item: item[1][1], reverse=True)
-
-        for key, value in drop_dict:
-            spaces_needed = 56 - len(f"{value[0]} x {key}({scapify.int_to_gp(value[1])})")
-            result = f"{ftext +fred}{value[0]} x{fend} {key}{' ' * spaces_needed}{ftext + fgreen}({scapify.int_to_gp(value[1])}){fend}\n"
-            if len(drop_block) + len(result) > 1021:
-                break
-            drop_block += result
-        drop_block += "```"
-
-        kills = database.get_killcount_by_team_id(team.team_id)
-        kill_dict = {}
-        for kill in kills:
-            kill = db_entities.Killcount(kill)
-            kill_dict[kill.boss_name] = kill.kills
-
-        kill_block = "```ansi\n"
-        kill_dict = sorted(kill_dict.items(), key=lambda item: item[1], reverse=True)
-        for key, value in kill_dict:
-            spaces_needed = 56 - len(f"{key}{value}")
-            result = f"{ftext + fred}{key}{fend}{' ' * spaces_needed}{ftext + fblue}{value}\n{fend}"
-            if len(kill_block) + len(result) > 1021:
-                break
-            kill_block += result
-        kill_block += "```"
-
-        team_members = database.get_players_by_team_id(team.team_id)
-        player_block = "```ansi\n"
-        rank = 0
-        for player in team_members:
-            rank = rank + 1
-            player = db_entities.Player(player)
-            spaces_needed = 56 - len(f"Rank {rank}: {player.player_name[:40]}") - len(f"{player.tiles_completed} tiles ({scapify.int_to_gp(player.gp_gained)})")
-            result = f"{ftext + fred}Rank {rank}:{fend} {player.player_name[:40]}{' ' * spaces_needed}{ftext + fblue}{player.tiles_completed} tiles {fend}{ftext + fgreen}({scapify.int_to_gp(player.gp_gained)})\n{fend}"
-            if len(player_block) + len(result) > 1021:
-                break
-            player_block += result
-        player_block += "```"
-
-        embed.add_field(name="Players", value=player_block, inline=False)
-        embed.add_field(name="Drops", value=drop_block, inline=False)
-        embed.add_field(name="Kills", value=kill_block, inline=False)
-        await ctx.respond(embed=embed)
+        server_ip = os.getenv('SERVER_IP')
+        team_url = f"http://{server_ip}/user/team/{team_name}"
+        await ctx.respond(team_url)
 
 
 
