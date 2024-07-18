e62a42c8b41315d66257a297ef9291289f3da6c1
Danny Decker <u1196904@utah.edu>
2024-07-18 16:33:30 -0700

    forgot to strip item names for sets

--- utils/bingo.py
+++ utils/bingo.py
@@ -59,6 +59,7 @@ def get_set_progress(tile_progress):
 
     for set in tile.tile_triggers.split('/'):
         for item in set.split(','):
+            item = item.strip()
             drops = database.get_drops_by_item_name_and_team_id(item, team.team_id)
             if len(drops) <= tile_completion_count:
                 missing_items.append(item)
