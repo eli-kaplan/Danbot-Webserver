8fe3373f93246e0f783638cbc2df1465c7c6e36e
AlmostEvil665 <u1196904@utah.edu>
2024-08-03 21:54:24 -0600

    added an approved guild list

--- bot.py
+++ bot.py
@@ -12,6 +12,20 @@ bot = discord.Bot()
 token = os.getenv('DISCORD_BOT_TOKEN')
 
 
+@bot.event
+async def on_guild_join(guild):
+    # Your code here
+    approved_guilds = set()
+
+    # Approved guilds
+    approved_guilds.add(369695042740420608)
+    approved_guilds.add(1216228320807485511)
+
+    if guild.id not in approved_guilds:
+        await guild.leave()
+
+
+
 
 def run():
     bot.add_cog(UserCog(bot))
